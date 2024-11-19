8c4795f252d57582ef5eba77db30f5526ec5ba89
ArefAH <arefabouhamdan@gmail.com>
2024-11-19 04:57:30 +0200

    PHP - Fixed create assignment api ( removed user_id)

--- backend/assignments/create.php
+++ backend/assignments/create.php
@@ -1,30 +1,18 @@
 <?php
-include 'connection.php';
-require "vendor/autoload.php";
+include '../connection.php';
 
-use Firebase\JWT\JWT;
-use Firebase\JWT\Key;
-
-$secretKey = "SecretKey";
-$headers = getallheaders();
-$jwt = $headers["Authorization"];
-
-$key = new Key($secretKey, "HS256");
-$payload = JWT::decode($jwt, $key); 
-
-$instructor_id = $payload->userId;
 $course_id = $_POST['course_id'] ?? null;
 $title = $_POST['title'] ?? null;
 $description = $_POST['description'] ?? null;
 $due_date = $_POST['due_date'] ?? null;
 
-if ($instructor_id == null || $course_id == null || $title == null || $description == null || $due_date == null) {
+if ($course_id == null || $title == null || $description == null || $due_date == null) {
     http_response_code(400);
     echo json_encode(["message" => "All fields are required."]);
 }
 
-$query = $connection->prepare("INSERT INTO assignments (instructor_id, course_id, title, description, due_date, created_at) VALUES (?, ?, ?, ?, ?, NOW())");
-$query->bind_param("iisss", $instructor_id, $course_id, $title, $description, $due_date);
+$query = $connection->prepare("INSERT INTO assignments (course_id, title, description, due_date) VALUES ( ?, ?, ?, ?)");
+$query->bind_param("isss",  $course_id, $title, $description, $due_date);
 
 if ($query->execute()) {
     http_response_code(201);
