811784b6e6dbec535d80add5227e67f57cb32bc7
ArefAH <arefabouhamdan@gmail.com>
2024-11-19 02:21:08 +0200

    PHP - Fixed user id fetching

--- backend/userCourses.php
+++ backend/userCourses.php
@@ -1,12 +1,29 @@
 <?php
 include 'connection.php';
+require "vendor/autoload.php";
 
-$user_id = $_POST['user_id'] ?? null;
+use Firebase\JWT\JWT;
+use Firebase\JWT\Key;
 
-    if ($user_id == null) {
-        http_response_code(400);
-        die(json_encode(["message" => "Invalid user ID."]));
-    }
+$secretKey = "SecretKey";
+$headers = getallheaders();
+$jwt = $headers["Authorization"];
+
+$key = new Key($secretKey, "HS256");
+$payload = JWT::decode($jwt, $key);
+
+if($payload->user_type != 'student') {
+    http_response_code(401);
+
+    echo json_encode([
+      "message"=> "Unauthorized"
+    ]);
+
+    return;
+}
+
+$user_id = $payload->userId;
+
 
     $query = $connection->prepare("SELECT c.course_id, c.title, c.description FROM enrollments e JOIN courses c ON e.course_id = c.course_id WHERE e.user_id = ?");
     $query->bind_param("i", $user_id);
