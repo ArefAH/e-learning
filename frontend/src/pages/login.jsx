08cedb9fe9bb6b7b95f1b1f8eb72c03ffcac077a
ArefAH <arefabouhamdan@gmail.com>
2024-11-20 00:42:26 +0200

    CSS - Fixed styling

--- frontend/src/pages/login.jsx
+++ frontend/src/pages/login.jsx
@@ -1,18 +1,19 @@
 import React from 'react'
 import Input from '../components/base/Input'
 import Button from '../components/base/Button'
+import '../styles/Login.css'
 import { useState } from 'react'
 
 const Login = () => {
   const [username , setUsername] = useState('')
   const [password , setPassword] = useState('')
   return (
-    <>
-    <h1>login</h1>
+    <div className='login'>
+    <h1>LOGIN</h1>
     <Input text={'Enter Username'} type={'text'} onChange={(e)=>{setUsername(e.target.value)}}/>
     <Input text={'Enter Password'} type={'password'} onChange={(e)=>{setPassword(e.target.value)}}/>
-    <Button onClick={console.log(username, password)}>Login</Button>
-    </>
+    <Button onClick={()=>{alert(username, password)}}>LOGIN</Button>
+    </div>
   )
 }
 
