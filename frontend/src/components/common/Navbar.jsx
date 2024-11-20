63ddaa7c8108e4c610edbec99ef1cbf9d55c2a02
ArefAH <arefabouhamdan@gmail.com>
2024-11-20 10:45:19 +0200

    React - Added navbar component

---
+++ frontend/src/components/common/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Logo from './../../assets/images/logo.png'
+import '../../styles/Navbar.css'
+
+const Navbar = () => {
+  return (
+    <div className='nav-bar'>
+      <img src={Logo} alt="Logo" />
+      <ul>
+        <p>Courses</p>
+        <p>Announcements</p>
+        <p>Assignments</p>
+      </ul>
+    </div>
+  )
+}
+
+export default Navbar
