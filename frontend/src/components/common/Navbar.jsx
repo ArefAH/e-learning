2cda59e0d5722b62c916bcf91105eca1dbac0e6c
ArefAH <arefabouhamdan@gmail.com>
2024-11-21 01:20:37 +0200

    React - Added sign out function

--- frontend/src/components/common/Navbar.jsx
+++ frontend/src/components/common/Navbar.jsx
@@ -1,14 +1,19 @@
 import React from 'react'
 import Logo from './../../assets/images/logo.png'
 import Button from '../base/Button'
+import {useNavigate} from 'react-router-dom'
 import '../../styles/Navbar.css'
 
 const Navbar = () => {
-
+  const navigate = useNavigate();
+  const handleSignOut = () => {
+    window.localStorage.clear();
+    navigate('/')
+  }
   return (
     <div className='nav-bar'>
       <img src={Logo} alt="Logo" />
-      <Button>Sign Out</Button>
+      <Button onClick={handleSignOut}>Sign Out</Button>
     </div>
   )
 }
