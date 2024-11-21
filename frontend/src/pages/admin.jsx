11191d634d6c45b45673134afd06f05473df1a9e
ArefAH <arefabouhamdan@gmail.com>
2024-11-21 14:06:17 +0200

    React - Added admin components

--- frontend/src/pages/admin.jsx
+++ frontend/src/pages/admin.jsx
@@ -1,10 +1,37 @@
-import React from 'react'
-import Navbar from '../components/common/Navbar'
+import React from "react";
+import Navbar from "../components/common/Navbar";
+import Input from "../components/base/Input";
+import Button from "../components/base/Button";
 
 const Admin = () => {
   return (
-    <><Navbar home={'admin'} /></>
-  )
-}
+    <>
+      <Navbar home={"admin"} />
+      <div className="users">
+        <h2>Users</h2>
+        <table>
+          <th>
+            <td>User Name</td>
+          </th>
+          <th>
+            <td>User Type</td>
+          </th>
+          <tbody>
+            <tr>
+              <td>User</td>
+              <td>Student</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+      <div>
+        <h2>Add Instructors</h2>
+        <Input />
+        <Input />
+        <Button>Add Instructor</Button>
+      </div>
+    </>
+  );
+};
 
-export default Admin
+export default Admin;
